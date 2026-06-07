40254dd66e9fccf38c4af3bc2240f959339b11c2
加藤瞳 <katohitomi@MacBook-Pro.local>
2026-06-07 21:56:55 +0900

    fix nav

--- app/components/MapView.jsx
+++ app/components/MapView.jsx
@@ -1,6 +1,18 @@
 'use client'
-import Map from 'react-map-gl/mapbox'
+import Map, { useMap } from 'react-map-gl/mapbox'
 import 'mapbox-gl/dist/mapbox-gl.css'
+import { useEffect } from 'react'
+
+function FlyToRegistrar() {
+  const { current: map } = useMap();
+  useEffect(() => {
+    if (!map) return;
+    window.__mapboxFlyTo = (lng, lat) => {
+      map.flyTo({ center: [lng, lat], zoom: 15 });
+    };
+  }, [map]);
+  return null;
+}
 
 export default function MapView({ children }) {
   return (
@@ -14,6 +26,7 @@ export default function MapView({ children }) {
       style={{ width: '100%', height: '100%' }}
       mapStyle="mapbox://styles/kthhtm/cmptvkv4f006501su5hqm3cp2"
     >
+      <FlyToRegistrar />
       {children}
     </Map>
   )
